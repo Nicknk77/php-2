64141f5c6c6f6bcbf856ace5a614576b726ea60a
Nikolay Kutepov <nkutepov@hotmail.com>
2023-02-25 23:15:39 +0300

    a little modify

--- cli.php
+++ cli.php
@@ -14,7 +14,7 @@ include __DIR__ . '/vendor/autoload.php';   // абсолютный путь
 
 function load($className) {
     $file = $className . '.php';
-    $file = str_replace('Geekbrains', 'src', $file);
+    $file = str_replace('Geekbrains\LevelTwo', 'src', $file);
     $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
     if (file_exists($file)) require_once $file;
 }
